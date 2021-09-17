8c99a6db577553c6c2fbf557b1062dd94f9b19b3
JaneGame <fibi92@mail.ru>
2021-09-17 23:51:37 +0300

    VK1

---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,45 @@
+package ru.netology;
+
+public class Post {
+    private String id;
+    private String text;
+    private String urlImage;
+    private String date;
+    private int heart;
+    private int comment;
+    private int repost;
+    private boolean right;
+
+    public void getVision(String id, String text, String urlImage, String date, int heart, int comment, int repost) {
+        return;
+    }
+
+    public void setIdText(){
+        this.id=id;
+    }
+
+    public void setVisionText(){
+        this.text=text;
+    }
+
+    public void urlImageVision(){
+        this.urlImage=urlImage;
+    }
+
+    public void setDateText(){
+        this.date=date;
+    }
+
+    public void setCommentText(){
+        this.comment=comment;
+    }
+
+    public void setHeartText(){
+        this.heart=heart;
+    }
+
+    public void setRepostText(){
+        this.repost=repost;
+    }
+
+}
